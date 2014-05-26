9ae92cd3b196cab8035b739aa76a35ac17bc8a09
Sakares Saengkaew <jobbeins@gmail.com>
2014-05-26 20:56:09 +0700

    add promotion rule with unittest

--- lib/supermarket.py
+++ lib/supermarket.py
@@ -1,6 +1,14 @@
 class SuperMarket(object):
-	
-	@staticmethod
-	def get_can_of_bean_prices():
-		return 0.65
+
+    @staticmethod
+    def get_can_of_bean_prices():
+        return 0.65
+
+    @staticmethod
+    def cost(number_of_can):
+
+        if number_of_can == 3:
+            return 1.0
+        else:
+            return number_of_can * 0.65
 
--- test/test_supermarket.py
+++ test/test_supermarket.py
@@ -4,14 +4,26 @@ import sys
 sys.path.append("../")
 from lib.supermarket import SuperMarket
 
+
 class TestSuperMarket(unittest.TestCase):
+
     """ TestSuperMarket class """
     
     def setUp(self):
       
-      self.can_of_bean_prices = 0.65
+        self.can_of_bean_prices = 0.65
       
     def test_can_of_beans_cost(self):
       
-      self.assertEqual(SuperMarket().get_can_of_bean_prices(), 0.65,
+        self.assertEqual(SuperMarket().get_can_of_bean_prices(), 0.65,
                          "Incorrect Prices")
+
+    def test_promotion_three_for_dollar(self):
+
+        self.assertEqual(SuperMarket().cost(3), 1.0,
+                         "Expected to have promotion three for a dollar")
+
+    def test_non_promotion(self):
+
+        self.assertEqual(SuperMarket().cost(2), 1.3,
+                         "Expected to have promotion three for a dollar")
